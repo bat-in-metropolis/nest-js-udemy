eedf65159ae8da05ea529f560fe7f2b252534277
Bat In Metropolis <rmnsdeo@gmail.com>
2026-01-17 21:16:58 +0530

    feat: implement basic CRUD operations for users and add corresponding HTTP request files

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete } from "@nestjs/common";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+	@Get()
+	public getUsers() {
+		return "You sent a get request to /users endpoint";
+	}
+
+	@Post()
+	public createUser() {
+		return "You sent a post request to /users endpoint";
+	}
+
+	@Patch()
+	public updateUser() {
+		return "You sent a patch request to /users endpoint";
+	}
+
+	@Put()
+	public replaceUser() {
+		return "You sent a put request to /users endpoint";
+	}
+
+	@Delete()
+	public deleteUser() {
+		return "You sent a delete request to /users endpoint";
+	}
+}
