9bd630a5e327e45c39df9c2531807defceae3983
Bat In Metropolis <rmnsdeo@gmail.com>
2026-01-24 18:48:09 +0530

    feat: Update PostsService to fetch user data via UsersService and return structured post objects.

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { UsersService } from "src/users/providers/users.service";
 
 @Injectable()
 export class PostsService {
+	constructor(
+		/**
+		 * Injecting UsersService
+		 */
+		private readonly usersService: UsersService,
+	) {}
+
 	findAll(userId: string) {
-		console.log("post service", { userId });
-		return "This action returns all posts";
+		// User Service
+		// Find a user with the provided userId
+		const user = this.usersService.findOneById(userId);
+
+		return [
+			{
+				id: 1,
+				title: "Post 1",
+				content: "Content 1",
+				user,
+			},
+			{ id: 2, title: "Post 2", content: "Content 2", user },
+		];
 	}
 }
