ad00906215bb3f5937dfe664cf155fe48d8df461
Bat In Metropolis <rmnsdeo@gmail.com>
2026-01-24 16:26:25 +0530

    feat: Add GET /posts/:userId endpoint and corresponding service method to retrieve user-specific posts.

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+	findAll(userId: string) {
+		console.log("post service", { userId });
+		return "This action returns all posts";
+	}
+}
